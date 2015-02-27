1e24f153b32f8520bc0cb73e7629043646098574
Tomer <t.gabay@student.rug.nl>
2015-02-27 13:30:53 +0100

    Volledig en succesvol uitgevoerd

---
+++ blood-die_identifier.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+import sys
+import json
+from collections import namedtuple
+
+def main(argv):
+	inputData= open(argv[1],'r')
+	outputFile=open('blood-die_identical.json','w')
+	activeFile = json.load(inputData)
+	CountryInfo =namedtuple('outputTuple', 'country, classification, blood, dying')
+	for line in activeFile:
+		currentTuple = CountryInfo(line[0],line[1],line[2], str(line[3]).rstrip()) #.rstrip() verwijdert \n
+		bloodWords = currentTuple.blood.split(',')
+		dyingWords = currentTuple.dying.split(',')
+		[json.dump(currentTuple, outputFile, indent=0) for blood in bloodWords if blood in dyingWords]
+	inputData.close()
+	outputFile.close()
+		
+		
+if __name__ == "__main__":
+	main(sys.argv)
+
